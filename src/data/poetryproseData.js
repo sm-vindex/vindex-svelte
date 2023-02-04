a71b6fbcc1c59aa92d2695e4fe3824cd8aab5ad9
Ariel <ariel.kerry@gmail.com>
2023-02-03 20:28:08 -0500

    update formatting ode poem & dear mama poem

--- src/data/poetryproseData.js
+++ src/data/poetryproseData.js
@@ -1,29 +1,33 @@
 export const poetryproseContent = [
-  `Dear Mama... 
-My magical cocoon, the safe haven within the home walls of childhood Where rose-tinted flowers grew beside hope-laden trees, 
-and n'er a mention of the wilderness beyond. 
-A precious whisper with the sweetness of honey flowing from your lips, and the story (of my life) began with a prophetic... 
-'Once upon a time' 
-As I grew, so my spellbound palace slowly disintegrated 
-before my eyes 
-Shattered dreams, irredeemable. Their shards piercing my heart. Sleeping Beauty and Snow White 
-stayed mute in their slumber, 
-never kissed and awoken, 
-No shape-shifting frogs. 
-Knights in shining armour remain stupefied in picture books, 
-Heroes are not interested in rescuing damsels in distress. 
-Imaginary creatures are too busy to save the day. 
-Neverland never existed, 
-Wendy’s dreams were cut short, while she looked on in vain. 
-The falsehood fairy tales that corrupted my childhood 
-were nightmarish fantasies. 
-Mama, your lies and deceit shredded my self-belief. 
-Those hidden scabs of supposed 'nurture' 
-remain unhealed, 
-Untouched by 'happily ever afters' 
-There is no easy ending to this story, no neat tidying up of loose ends. Instead, I wade deep through a murky reality, 
-Holding out for a miracle that your lies are real.`,
-`Our journey stays a priceless memory
+  `Dear Mama...
+My magical cocoon, the safe haven within the home walls of childhood
+    Where rose-tinted flowers grew beside hope-laden trees,
+          and n’er a mention of the wilderness beyond.
+A precious whisper with the sweetness of honey flowing from your lips,
+    and the story (of my life) began with a prophetic...
+    ’Once upon a time’
+        As I grew, so my spellbound palace slowly     disintegrated
+                                                                                before my eyes
+              Shattered    dreams, irredeemable. Their shards piercing my heart.
+                                          Sleeping Beauty and Snow White
+                                      stayed mute in their slumber,
+                                            never kissed and awoken,
+                              No shape-shifting frogs.
+                    Knights in shining armour remain stupefied in picture books,
+                          Heroes are not interested in rescuing damsels in distress.
+                    Imaginary creatures are too busy to save the day.
+                Neverland never existed,
+                      Wendy’s dreams were cut short, while she looked on in vain.
+        The falsehood fairy tales that corrupted my childhood
+              were nightmarish fantasies.
+      Mama, your   lies   and   deceit shredded my self-belief.
+            Those hidden scabs of supposed ’nurture’
+                      remain unhealed,
+      Untouched by ’happily ever afters’
+          There is no easy ending to this story, no neat tidying up of loose ends.
+                            Instead, I wade deep through a murky reality,
+                        Holding out for a miracle that your lies are                   real.`,
+  `Our journey stays a priceless memory
 I promised my return to continue the story
 Then why is it that I feel the sorrow
 That I’ll no longer be in your tomorrow.
@@ -38,7 +42,7 @@ The moments embrace me forever.
 I long, hanging on the verge
 
 Upon the glistening of the horizon your smiles emerge.`,
-`When my abuela was young, she had a dream,
+  `When my abuela was young, she had a dream,
 She dreamed of a future where her children wouldn’t be called stupid by their teachers for
 painting purple jacaranda trees,
 She dreamed of her children living out their dreams as scholars, not toiling as gardeners all day,
@@ -70,7 +74,7 @@ My abuela and my abuela’s daughter had dreams,
 My abuela and my abuela’s daughter worked hard,
 I became a product of their dreams,
 I will work to honor their dreams.`,
-`because suits are uncomfortable. (So are dresses) 
+  `because suits are uncomfortable. (So are dresses) 
 I want to die happy 
 whatever that means. I want to run
 away 
@@ -100,60 +104,86 @@ bursting out      unfurling.
 
 Happiness. Freedom. Sweatpants. (They are the same)
 `,
-`in the cold dank space 
-of a basement room, 
-she sat surrounded by the echoes of previous lives, yet so very 
-alone. 
-head down 
-shoulders sagging 
-drained eyes 
-sad, wispy sighs. 
-carefully lifted the pen: 
-a living antique, 
-weighty like a confession.
+  `in the cold dank space
+of a rundown, long-abandoned basement room,
+she sat surrounded by the echoes of previous lives,
+yet so very
+alone.
+head down
+shoulders sagging
+drained eyes
 
-ink sinks, 
-coursing through the timelines that have defined her life she walks discreetly
-presses the paper to her lips, 
-in silent prayer 
-as it carries the outpourings from her heart 
-across the corridors 
-of a past 
-a drop of ink on paper 
-(a haunting mistake from yesteryear) 
-hesitates 
-then smudges the paper 
-overwhelmed by pain, 
-slicing through ancient
-the droplet stains, succumbs to lost nostalgia.
-painstakingly written 
+sad, wispy sighs.
 
-an emotional shipwreck 
-buried deep beneath a sonnet of grief 
-then 
-warmed by 
-next 
-a spark from the hearth 
-reignites the slow burn fire 
-flames fanned by the winds of change 
-vestiges of hope warm your heart, 
-lighten your load. 
-the vintage pen honors 
-your word 
-your voice 
-your world 
-casting ancient shapes no 
-silence can redeem 
-pearls of wisdom lie dormant for aeons 
-hidden
-the brave spirit of 
-the survivor, nay, thriver emerges, 
-a treasure trove of inspiration 
-the venerable pen speaks only the truth it finds weaving words into poems, 
-vividly portraying pain to passion 
+carefully lifted the timeworn pen:
+pockmarked with use, a living antique,
+weighty like a confession in her hand.
+
+blackened ink sinks,
+coursing through the timelines that have defined her life
+on yellowed paper
+with threadbare edges that have seen better days.
+
+she walks discreetly, clasping the folded remnants of long lost pain
+gently presses the paper to her lips,
+in silent prayer
+as
+
+it carries the outpourings from her heart
+across
+the contorted corridors
+of a shredded past
+
+a drop of ink on paper
+(a haunting mistake from yesteryear)
+hesitates
+then smudges the paper
+overwhelmed by rivulets of pain,
+slicing through ancient bedrock
+the droplet stains, succumbing to lost memories,
+buried in waves of nostalgia.
+
+streaming ink as dripping blood from an open wound
+each letter a note from the soul
+painstakingly written
+tracing in forensic detail long forgotten twists and turns,
+now mere remnants
+reflecting your inner tears
+
+fathomless depths of your torn soul
+subdued by countless words
+imbibed with layered meanings
+an emotional shipwreck
+buried deep beneath a sonnet of grief
+then
+warmed by
+glowing coals,
+next
+a spark from the hewn hearth
+reignites the slow burn fire
+flames fanned by the winds of change
+
+vestiges of hope warm your heart,
+lighten your load.
+the vintage pen honors
+your word
+your voice
+your world
+casting ancient shapes no
+silence can redeem
+
+pearls of wisdom lie dormant for aeons
+hidden in womblike darkness
+the brave spirit of
+the survivor, nay, thriver emerges,
+a treasure trove of inspiration
+
+the venerable pen speaks only the truth it finds
+weaving words into poems,
+vividly portraying pain to passion
 on a journey underlined by the
-bleeding ink of pen
-	`,
+bleeding ink of pen`,
+
   `You stood in the chilly aisle of a well-lit pet store, bright lights straining your eyes. You’ve come here way too often, always for that one cat. Today feels different to you, though, and you creep up to the cat’s little crate. The contents inside were a few scattered toys and a blanket that had clearly seen better days. The nameplate read “Lucas, 2, Male, Friendly and excited to meet my furever family!”, and you scoffed at the mediocre advertising. Still, you glanced at Lucas, unable to help yourself, and watched the small, striped gray cat blink back at you. His sharp, golden eyes met yours in a hopeful glint and you found yourself quietly mewling back as if he could understand how badly you wanted to get him out of there. 
 Fortunately, you’d received a paycheck from your typical nine to five job last week with a bit of a bonus sprinkled in, an offering once left dangling as an incentive to keep working hard. As soon as you got it, it was safely sealed in your bag, ready for that very moment. You were in a spending mood, and in a moment of faith, called the front desk to purchase Lucas, whipping out the respective cash and forms. 
 Exactly one hour and 19 minutes later, you had a cat sitting in the backseat of your car, absolutely howling his tiny lungs out in confusion. As you entered your drab flat, you grabbed the cat carrier with gentleness that was uncharacteristic to you, and calmly set out the basics for Lucas- food, water, litter boxes, toys. 
@@ -195,7 +225,8 @@ You had to move on, though. For him and for you. You distract yourself everyday,
 
 You made sugar cookies complete with horrendous attempts of decorations, sprinkles littering your well-used kitchen table. You hung up an ornament in honor of Lucas on that same plastic tree, and watched the littering of snowflakes on the frosty ground. Lifting your hot chocolate in a salute, you whispered into the delicate air, “Thank you, buddy. You made life worth living.”	
 `,
-  `The book that was once so precariously placed on the shelf tipped backward and splayed itself open on my feet. The force of the impact cracked the spine, causing multiple pages to fly out, and flutter in the air around me. I stared at the pages in motion and realized how they will never return back to the book that they once were, bound together by glue and thread. Even if I pick up the pages from the floor and attempt to put them back as they were, they will never fit quite the same again. Like an altered jigsaw piece with no match or an ill-fitting item of clothing, grown out of and worn out. Just like how we will never return back to who we once were, destined to an ever-changing state that is life. Colors vary in their brightness (or lack thereof) throughout time, never appearing the same again. Sharp edges lose their corners and soft sides gain their rough points. I am not the person I was yesterday, the day before, or the month before that. And I never will be again, so how could I ever look the past in the eye and expect to be the same again?`, `I woke up on a Friday morning. I was preparing for the weekend. It was very cold. I thought the farm lost power again. I blindly tried to find extra blankets to stay warm. After accepting that it was not going to get any warmer, I prepared myself to rush out of bed to put some clothes on. Frantically pulling a pink hoodie over my head, I noticed that my window had been left open all night. My cat loved to listen to the snow falling and to the rabbits maneuvering through the snow, so I had forgotten to close it. However, I had very little time to dwell on it as I was putting together an overnight bag. The snow was normal but the storm canceled school for two days. My parents could not take another day off work for me so I had to go over to my grandmother’s house. I ate my breakfast slowly, as I did not have to rush to the bus like I usually do. My dad took the time to make me an omelet with what looked like onions, peppers, and ham. I finished the food and walked over to my grandmother’s house. At her house, the smell always comforted me. There was never anything to worry about–no stress, no anxiety, and most importantly, no homework. My grandmother always made mac and cheese for me with the noodles that I liked. The noodles that looked like a swine's tail, all swirly. Her favourite pastime was collecting pigs. Ceramics, stuffed toys, pictures–you name it. With so many in sight throughout her home, they always made me chuckle a little. Why did she like pigs so much?
+  `The book that was once so precariously placed on the shelf tipped backward and splayed itself open on my feet. The force of the impact cracked the spine, causing multiple pages to fly out, and flutter in the air around me. I stared at the pages in motion and realized how they will never return back to the book that they once were, bound together by glue and thread. Even if I pick up the pages from the floor and attempt to put them back as they were, they will never fit quite the same again. Like an altered jigsaw piece with no match or an ill-fitting item of clothing, grown out of and worn out. Just like how we will never return back to who we once were, destined to an ever-changing state that is life. Colors vary in their brightness (or lack thereof) throughout time, never appearing the same again. Sharp edges lose their corners and soft sides gain their rough points. I am not the person I was yesterday, the day before, or the month before that. And I never will be again, so how could I ever look the past in the eye and expect to be the same again?`,
+  `I woke up on a Friday morning. I was preparing for the weekend. It was very cold. I thought the farm lost power again. I blindly tried to find extra blankets to stay warm. After accepting that it was not going to get any warmer, I prepared myself to rush out of bed to put some clothes on. Frantically pulling a pink hoodie over my head, I noticed that my window had been left open all night. My cat loved to listen to the snow falling and to the rabbits maneuvering through the snow, so I had forgotten to close it. However, I had very little time to dwell on it as I was putting together an overnight bag. The snow was normal but the storm canceled school for two days. My parents could not take another day off work for me so I had to go over to my grandmother’s house. I ate my breakfast slowly, as I did not have to rush to the bus like I usually do. My dad took the time to make me an omelet with what looked like onions, peppers, and ham. I finished the food and walked over to my grandmother’s house. At her house, the smell always comforted me. There was never anything to worry about–no stress, no anxiety, and most importantly, no homework. My grandmother always made mac and cheese for me with the noodles that I liked. The noodles that looked like a swine's tail, all swirly. Her favourite pastime was collecting pigs. Ceramics, stuffed toys, pictures–you name it. With so many in sight throughout her home, they always made me chuckle a little. Why did she like pigs so much?
 At least, I knew why I liked pigs so much. They always remind me of her.`,
 ];
 
